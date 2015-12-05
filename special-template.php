28fd5f3d8a1495b5200f4c42b7d9549868e75287
Luis Edo Gonzalez O <luis.gonzalez@nuncode.co>
2015-12-05 11:04:36 -0500

    Menu, templates y funciones

--- special-template.php
+++ special-template.php
@@ -1,11 +1,16 @@
 <?php
+
+/*
+Template Name: Special Layout
+*/
+
 get_header();
 
 	if (have_posts()):
 		while (have_posts()) : the_post();
 		?>
-			<article class="post">
-				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<article class="post page">
+				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 			</article>
 		<?php endwhile;
