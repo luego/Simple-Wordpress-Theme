929597189f5ea42f62339cc0e6ba882b1fe5294a
Luis Edo Gonzalez O <luego1986@gmail.com>
2015-12-05 19:52:00 -0500

    Cambios en index y se agrego el archivo de archive.php

--- archive.php
+++ archive.php
@@ -2,6 +2,31 @@
 get_header();
 
 	if (have_posts()):
+
+		?>
+
+		<h2><?php  
+
+		if (is_category()) {
+			single_cat_title();
+		}elseif (is_tag()) {
+			single_tag_title();
+		}elseif (is_author()) {
+			the_post();
+			echo 'Author Archives: ' . get_the_author();
+			rewind_posts();
+		}elseif (is_day()) {
+			echo 'Daily Archives: ' . get_the_date();
+		}elseif (is_month()) {
+			echo 'Montly Archives: ' . get_the_date('F Y');
+		}elseif (is_year()) {
+			echo 'Yearly Archives: ' . get_the_date('Y');
+		}else
+			echo 'Archives';
+
+		?></h2>
+
+		<?php
 		while (have_posts()) : the_post();
 		?>
 			<article class="post">			
@@ -24,7 +49,7 @@ get_header();
 					}
 				?>
 				</p>
-				<?php the_content(); ?>
+				<?php the_excerpt(); ?>
 			</article>
 		<?php endwhile;
 	else :
